c7ca19cf7a258865233eddc06c84e3f0245b592c
Edward Dume <edwarddume55@gmail.com>
2025-04-25 15:04:35 +0300

    store and carts

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 ]  
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
