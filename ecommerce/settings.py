d35130a8c11c01fde48e649dfd5f0d015d3a3fbb
Edward Dume <edwarddume55@gmail.com>
2025-04-28 19:39:38 +0300

    user registration, login, logout and dashbord

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -125,6 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+
+
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'static'
 # STATICFILES_DIRS =[
@@ -142,3 +144,18 @@ MEDIA_ROOT = BASE_DIR/'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+
+#SMTP CONFIGURATION
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'edwarddume55@gmail.com'
+EMAIL_HOST_PASSWORD = 'uaaj qycf plhm cigo'
+EMAIL_USE_TLS = True
